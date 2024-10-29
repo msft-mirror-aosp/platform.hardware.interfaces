335015939hi/dumbos-platform_hardware_interfaces

Add T4T Ndef Nfceee feature support
Bug: 339117870
Test: Manual
Change-Id: Ifcfe74caf6c14a8295435ecacbb9a19156ac3842

nfc/aidl/aidl_api/android.hardware.nfc/current/android/hardware/nfc/NfcConfig.aidl
@@ -50,4 +50,5 @@ parcelable NfcConfig {
   byte[] offHostRouteEse;
   byte defaultIsoDepRoute;
   byte[] offHostSimPipeIds = {};
+  byte t4tNfceeEnable;
 }

nfc/aidl/android/hardware/nfc/NfcConfig.aidl
@@ -90,4 +90,8 @@ parcelable NfcConfig {
      * Pipe IDs for UICC. Empty if not available
      */
     byte[] offHostSimPipeIds = {};
+    /**
+     * T4T NFCEE feature configuration
+     */
+    byte t4tNfceeEnable;
 }

msft-mirror-aosp/platform.hardware.interfaces

nfc/aidl/aidl_api/android.hardware.nfc/current/android/hardware/nfc/NfcConfig.aidl
@@ -50,4 +50,5 @@ parcelable NfcConfig {
   byte[] offHostRouteEse;
   byte defaultIsoDepRoute;
   byte[] offHostSimPipeIds = {};
+  boolean t4tNfceeEnable;
 }

nfc/aidl/android/hardware/nfc/NfcConfig.aidl
@@ -90,4 +90,8 @@ parcelable NfcConfig {
      * Pipe IDs for UICC. Empty if not available
      */
     byte[] offHostSimPipeIds = {};
+    /**
+     * T4T NFCEE feature configuration
+     */
+    boolean t4tNfceeEnable;
 }