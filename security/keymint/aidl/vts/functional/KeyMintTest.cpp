8a00ef2caee20dce854aeb0557c30e4ebe6af51a
Catherine Vlasov <cvlasov@google.com>
2026-09-08 02:55:57 -0700

    Update properties referenced in Vsr14Test skip condition.
    GMS-VSR-3.10-013.001 is only prefixed with "DEVICEs
    launching with Android 14" and does not reference any
    vendor/board API level properties. However, those
    properties should also be checked to ensure that devices
    that launched with a chipset "older" than the corresponding
    system version (with GRF) can pass.
    
    Bug: 552373772
    Test: atest VtsAidlKeyMintTargetTest
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:d5a87d5c0f00268aa5c3f901aa966aced4a1361a
    Merged-In: Icd5e950cf41330ea4583a5fa30fdb56dcfe2a478
    Change-Id: Icd5e950cf41330ea4583a5fa30fdb56dcfe2a478
    
    ATTENTION: Conflicts occurred while applying the patch.
    Please resolve conflict markers.
    
    Change-Id: I9a4483b5efe03216a79cb7e29987efebc7a25c27

--- security/keymint/aidl/vts/functional/KeyMintTest.cpp
+++ security/keymint/aidl/vts/functional/KeyMintTest.cpp
@@ -8535,9 +8535,15 @@ TEST_P(VsrRequirementTest, Vsr13Test) {
 }
 
 TEST_P(VsrRequirementTest, Vsr14Test) {
-    int vsr_api_level = get_vsr_api_level();
-    if (vsr_api_level < __ANDROID_API_U__) {
-        GTEST_SKIP() << "Applies only to VSR API level 34, this device is: " << vsr_api_level;
+    int product_first_api_level = get_product_first_api_level_or_fail();
+    int board_api_level = get_board_api_level_or_fail();
+    int board_first_api_level = get_board_first_api_level_if_available();
+    bool gms_vsr_condition = product_first_api_level >= 34 &&
+                             (board_api_level >= 34 || board_first_api_level >= 34);
+    if (!gms_vsr_condition) {
+        GTEST_SKIP() << "Applies only if product first API level (" << product_first_api_level
+                     << ") is >= 34 and either board API level (" << board_api_level
+                     << ") or board first API level (" << board_first_api_level << ") is >= 34";
     }
     EXPECT_GE(AidlVersion(), 3) << "VSR 14+ requires KeyMint version 3";
 }
